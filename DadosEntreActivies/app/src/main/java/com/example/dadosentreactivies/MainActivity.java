c2262da228f9a82edeb6ff4b5906d46f691f76c3
brino7 <brino7@hotmail.com>
2021-11-19 15:36:16 -0300

    Finalizado o aprendizado de Passando dados entre activity.

---
+++ DadosEntreActivies/app/src/main/java/com/example/dadosentreactivies/MainActivity.java
@@ -0,0 +1,37 @@
+package com.example.dadosentreactivies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button buttonEnviar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(getApplicationContext(), SegundaActivity.class);
+
+                //Passando dados
+                intent.putExtra("nome", "Anderson");
+                intent.putExtra("idade", 37);
+
+                startActivity( intent );
+
+            }
+        });
+
+    }
+}
